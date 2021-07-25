9270148d81bc7497e3dd5d01cfc93cb56123b393
Marcelo <mlimideiro@gmail.com>
2021-07-25 18:38:38 -0300

    Sección 5 - Navegacion

--- proyectogalery/src/App.js
+++ proyectogalery/src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Aurora from './components/Aurora';
+import Beach from './components/Beach';
+import Forest from './components/Forest';
+import Jungle from './components/Jungle';
+import Lake from './components/Lake';
+import Milky from './components/Milky';
 
 function App() {
   return (
-    <div className="">
-      <h1>hola mundo</h1>
-    </div>
+    <Router>
+      <Route path="/Aurora" component={Aurora} />
+      <Route path="/Beach" component={Beach} />
+      <Route path="/Forest" component={Forest} />
+      <Route path="/Jungle" component={Jungle} />
+      <Route path="/Lake" component={Lake} />
+      <Route path="/Milky" component={Milky} />
+      
+    </Router>
   );
 }
 
---
+++ proyectogalery/src/components/Navegacion.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const Navegacion = () => {
+    return (
+        <div>
+            <Link>
+            <figure>
+                <img src="" alt="" />
+                <figcaption>Aurora</figcaption>
+            </figure>
+            </Link>
+            
+            <Link>
+            <figure>
+                <img src="" alt="" />
+                <figcaption>Beach</figcaption>
+            </figure>
+            </Link>
+
+            <Link>
+            <figure>
+                <img src="" alt="" />
+                <figcaption>Aurora</figcaption>
+            </figure>
+            </Link>
+
+            <Link>
+            <figure>
+                <img src="" alt="" />
+                <figcaption>Aurora</figcaption>
+            </figure>
+            </Link>
+
+            <Link>
+            <figure>
+                <img src="" alt="" />
+                <figcaption>Aurora</figcaption>
+            </figure>
+            </Link>
+
+            <Link>
+            <figure>
+                <img src="" alt="" />
+                <figcaption>Aurora</figcaption>
+            </figure>
+            </Link>
+        </div>
+    )
+}
+
+export default Navegacion
